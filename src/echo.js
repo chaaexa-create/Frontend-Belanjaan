a8e42e51537576c7f14b0bb862e3b7083d1537e9
Alexx-20 <uchaminecraft2003@gmail.com>
2026-07-04 20:15:52 +0700

    websocket fiks

--- src/echo.js
+++ src/echo.js
@@ -4,13 +4,13 @@ import Pusher from 'pusher-js'
 window.Pusher = Pusher
 
 const echo = new Echo({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: import.meta.env.VITE_REVERB_PORT,
-  wssPort: import.meta.env.VITE_REVERB_PORT,
-  forceTLS: false,
-  enabledTransports: ['ws', 'wss'],
+    broadcaster: 'reverb',
+    key: 'ac46bb33b376bc5828e2', // Sesuai dengan VITE_PUSHER_APP_KEY kamu
+    wsHost: 'backend-belanjaan-production.up.railway.app', // PENTING: Ganti dengan domain asli backend Railway-mu tanpa https:// atau /api
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true, // Wajib TRUE untuk production web
+    enabledTransports: ['ws', 'wss'],
 })
 
 export default echo
